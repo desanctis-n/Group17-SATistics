b4bd80c967f932f1fb617ec2996c7a3d6ce5ca58
desanctis-n <126989824+desanctis-n@users.noreply.github.com>
2023-12-01 16:26:16 -0500

    Update Scores.cpp
    Altered File IO. There is now only one map of sortcriteria to values

--- Scores.cpp
+++ Scores.cpp
@@ -6,6 +6,10 @@ Scores::Report::Report() =default;
 Scores::Scores() {
     generateFromFile();
 }
+
+// ----------------------------- FILE IO ---------------------------- //
+
+
 void Scores::generateFromFile() {
     chrono::high_resolution_clock time;
     auto start = time.now();
@@ -14,17 +18,17 @@ void Scores::generateFromFile() {
     if (file.is_open()) {
         const int NUMCOLS = 99;
         const int NUMROWS = 578;
-        const int NUMCLASSMEMBERS = 6;
+        const int NUMCLASSMEMBERS = 4;
         string garbage;
         string temp;
-        static array<string*, NUMCOLS> names;
-        assignNames(file, names);
+        array<string*, NUMCOLS> labels;
+        assignNames(file, labels);
         for(int i = 0; i < NUMROWS - 1; i++) {
-            dataSet.emplace(i, new Report);
+            pair<int, string> key = getKey(file);
+            dataSet.emplace(key, new Report);
             for (int j = 0; j < NUMCLASSMEMBERS; j++)
-                assignDataMember(file, i, j);
-            assignDataMap(file, i, false, names);
-            assignDataMap(file, i, true, names);
+                assignDataMember(file, key, j);
+            assignDataMap(file, key, labels);
             getline(file, garbage, '\n');
         }
     }
@@ -33,63 +37,66 @@ void Scores::generateFromFile() {
     auto end = time.now();
     cout << chrono::duration_cast<chrono::milliseconds>(end-start).count() << endl;
 }
-void Scores::assignDataMember(ifstream &file, const int &i, const int &switcher) {
+pair<int, string> Scores::getKey(ifstream &file) {
+    string garbage;
+    string temp;
+    getline(file, garbage, '\"');
+    getline(file, temp, '\"');
+    int year = stoi(temp);
+    getline(file, garbage, '\"');
+    getline(file, temp, '\"');
+    return make_pair(year, temp);
+}
+void Scores::assignDataMember(ifstream &file, const pair<int, string> &key, const int &switcher) {
     string garbage;
     string temp;
     getline(file, garbage, '\"');
     getline(file, temp, '\"');
     switch (switcher) {
         case 0:
-            dataSet[i]->year = stoi(temp);
+            dataSet[key]->stateName = temp;
             break;
         case 1:
-            dataSet[i]->stateCode = temp;
+            dataSet[key]->totalMath = stoi(temp);
             break;
         case 2:
-            dataSet[i]->stateName = temp;
+            dataSet[key]->totalTestTakers = stoi(temp);
             break;
         case 3:
-            dataSet[i]->totalMath = stoi(temp);
-            break;
-        case 4:
-            dataSet[i]->totalTestTakers = stoi(temp);
-            break;
-        case 5:
-            dataSet[i]->totalVerbal = stoi(temp);
+            dataSet[key]->totalVerbal = stoi(temp);
         default:
             break;
     }
 }
-void Scores::assignDataMap(std::ifstream &file, const int &i, const bool &switcher, const array<std::string*, 99> &names) {
+void Scores::assignDataMap(std::ifstream &file, const pair<int, string> &key, const array<std::string*, 99> &names) {
     string garbage;
     string temp;
     const int NUMCOLS = 99;
     const int NUMGPACOLS = 18;
-    const int NUMCLASSMEMBERS = 6;
-    if (!switcher) {
-        for (int j = NUMCLASSMEMBERS; j < NUMGPACOLS; j++) {
-            getline(file, garbage, '\"');
-            getline(file, temp, '\"');
-            dataSet[i]->subjectToAvgGPAs.emplace(*names[j], stof(temp));
-        }
-    }
-    else {
-        for (int j = NUMGPACOLS; j < NUMCOLS; j++) {
-            getline(file, garbage, '\"');
-            getline(file, temp, '\"');
-            dataSet[i]->criteriaToScores.emplace(*names[j], stoi(temp));
-        }
-    }
-}
-void Scores::assignNames(std::ifstream &file, array<string*, 99> &names) {
-    string garbage;
-    string temp;
-    for (int i = 0; i < names.size(); i++) {
+    const int NUMCLASSMEMBERS = 4;
+    for (int j = NUMCLASSMEMBERS + 2; j < NUMCOLS; j++) {
         getline(file, garbage, '\"');
         getline(file, temp, '\"');
-        names[i] = new string(temp);
+        dataSet[key]->criteriaToValue.emplace(*names[j], stof(temp));
     }
 }
-void Scores::modifyDisplayVector(const Report &report) {
+void Scores::assignNames(std::ifstream &file, array<string*, 99> &labels) const {
+    string garbage;
+    string temp;
+    for (int i = 0; i < 99; i++) {
+        getline(file, garbage, '\"');
+        getline(file, temp, '\"');
+        labels[i] = new string(temp);
+    }
+}
+
+
+// ----------------------------- VECTOR MODIFICATION ---------------------------- //
+
+
+void Scores::modifyDisplayVector(const int &year, const string& stateCode) {
+
+}
+void Scores::heapSort(const std::string &sortCriteria) {
 
 }

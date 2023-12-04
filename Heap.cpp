9f29a066c6cf04bb13ebdcc8197cb2dbb7c425b6
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2023-12-04 17:47:36 -0500

    Completed Heap. Everything has been tested.

--- Heap.cpp
+++ Heap.cpp
@@ -20,74 +20,38 @@ vector<Scores::Report*> Scores::Heap::insertHeap(Scores::Report* val, string &so
 
 vector<Scores::Report*> Scores::Heap::extractMax(Scores::Report* &tempval, string &sortCriteria)
 {
+    if (size <= 1) {
+        return arr;
+    }
     tempval = arr[0];
     arr[0] = arr[size - 1];
     size--;
+    arr.resize(size);
     int index = 0;
-    int leftchild = (2*index) +1;
-    int rightchild = (2*index)+2;
-    Scores::Report* placeholder;
-    if(size <= 1){
-        return arr;
-    }
-    while((leftchild <= size || rightchild <= size) && ((arr[index]->criteriaToValue[sortCriteria] < arr[leftchild]->criteriaToValue[sortCriteria] || arr[index]->criteriaToValue[sortCriteria] < arr[rightchild]->criteriaToValue[sortCriteria]))){
-        if(leftchild <= size){ // If the left child is in range
-            if(arr[leftchild]->criteriaToValue[sortCriteria] > arr[index]->criteriaToValue[sortCriteria]){ //If the left child is greater than the parent
-                if(rightchild <= size){ //If the right child exists
-                    if(arr[rightchild]->criteriaToValue[sortCriteria] > arr[index]->criteriaToValue[sortCriteria]){ // if the right child also is greater than the parent
-                        if(arr[leftchild]->criteriaToValue[sortCriteria] > arr[rightchild]->criteriaToValue[sortCriteria]){ // if the left child is bigger than the right child
-                            placeholder = arr[index];
-                            arr[index] = arr[leftchild];
-                            arr[leftchild] = placeholder;
-                            index = leftchild;
-                            leftchild = (2*index) +1;
-                            rightchild = (2*index)+2;
-                            continue;
-
-                        } else if(arr[rightchild] > arr[leftchild]){ //if the right child is greater than the left child
-                            placeholder = arr[index];
-                            arr[index] = arr[rightchild];
-                            arr[rightchild] = placeholder;
-                            index = rightchild;
-                            leftchild = (2*index) +1;
-                            rightchild = (2*index)+2;
-                            continue;
-                        }
-                    }
-                }
-            }
-        } else if(rightchild <= size){ // If the right child is in range
-            if(arr[rightchild]->criteriaToValue.at(sortCriteria) > arr[index]->criteriaToValue.at(sortCriteria)){ //If the right child is greater than the parent
-                if(leftchild <= size){ //If the left child exists
-                    if(arr[leftchild]->criteriaToValue.at(sortCriteria) > arr[index]->criteriaToValue.at(sortCriteria)){ // if the left child also is greater than the parent
-                        if(arr[rightchild]->criteriaToValue.at(sortCriteria) > arr[leftchild]->criteriaToValue.at(sortCriteria)){ // if the right child is bigger than the left child
-                            placeholder = arr[index];
-                            arr[index] = arr[rightchild];
-                            arr[rightchild] = placeholder;
-                            index = rightchild;
-                            leftchild = (2*index) +1;
-                            rightchild = (2*index)+2;
-                            continue;
-                        } else if(arr[leftchild]->criteriaToValue.at(sortCriteria) > arr[rightchild]->criteriaToValue.at(sortCriteria)){ //if the left child is greater than the right child
-                            placeholder = arr[index];
-                            arr[index] = arr[leftchild];
-                            arr[leftchild] = placeholder;
-                            index = leftchild;
-                            leftchild = (2*index) +1;
-                            rightchild = (2*index)+2;
-                            continue;
-                        }
-                    }
-                }
-            }
+    int leftchild;
+    int rightchild;
+    bool loop = true;
+    while (loop) {
+        leftchild = 2 * index + 1;
+        rightchild = 2 * index + 2;
+        int maxChildIndex = leftchild;
+        if (rightchild < size && arr[rightchild]->criteriaToValue.at(sortCriteria) > arr[leftchild]->criteriaToValue.at(sortCriteria)) {
+            maxChildIndex = rightchild;
         }
-
+        if (maxChildIndex < size && arr[maxChildIndex]->criteriaToValue.at(sortCriteria) > arr[index]->criteriaToValue.at(sortCriteria)) {
+            swap(arr[index], arr[maxChildIndex]);
+            index = maxChildIndex;
+        } else {
+            break;
+        }
     }
     return arr;
 }
+
 vector<Scores::Report*> Scores::Heap::sortHeap(string &sortCriteria) {
+    arr.resize(size);
     vector<Scores::Report*> sortedlist;
-    sortedlist.resize(578);
+    sortedlist.resize(size);
     int listsize = 0;
     Scores::Report* tempval;
     Scores::Report* temparrayval;
